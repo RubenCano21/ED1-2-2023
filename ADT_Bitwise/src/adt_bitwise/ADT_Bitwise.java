1ba148f4e20ba7946edcce49c4daf082f21ebc94
Ruben Cano <ruben217.rc@gmail.com>
2023-08-17 21:28:38 -0400

    creando un nuevo proyecto para trabajar con Bitwise

---
+++ ADT_Bitwise/src/adt_bitwise/ADT_Bitwise.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package adt_bitwise;
+
+/**
+ *
+ * @author ruben
+ */
+public class ADT_Bitwise {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
